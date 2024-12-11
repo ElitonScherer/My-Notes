d7b1907dbf4d3de08c47f140abd10a337b37ff0e
ElitonScherer <elitonscherer@gmail.com>
2024-12-10 22:09:34 -0400

    step-2

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -27,6 +27,7 @@ class _LoginViewState extends State<LoginView> {
   void dispose() {
     _email.dispose();
     _password.dispose();
+    super.dispose();
 
   }
 
@@ -35,161 +36,147 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login',
-          style:TextStyle(
-             color: Colors.white,
-          ),
+      appBar: AppBar(title:const Text('Login',style: TextStyle(color: Colors.white),),backgroundColor: Colors.blueAccent,),
+      body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'Log In to Your Account',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
 
-        ),
-        backgroundColor: Colors.blueAccent,
-        shadowColor: Colors.black.withOpacity(0.8),
-        elevation: 5,
+                ),
+              ),
+              const SizedBox(height: 30),
 
-      ),
-      body: FutureBuilder(
-          future: Firebase.initializeApp(
-            options: DefaultFirebaseOptions.currentPlatform,
-          ),
-          builder: (context, snapshot) {
-            switch(snapshot.connectionState){
+              Container(
+                width: 300,
+                child: Column(
 
-              case ConnectionState.done:
-                return  Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Text(
-                        'Log In to Your Account',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 22,
-
-                        ),
-                      ),
-                      const SizedBox(height: 30),
-
-                      Container(
-                        width:300,
-                        child: Column(
-
-                          children: [
-                            TextField(
-                              controller: _email,
-                              enableSuggestions:false ,
-                              autocorrect: false,
-                              keyboardType: TextInputType.emailAddress,
-                              decoration: const InputDecoration(
-                                contentPadding: EdgeInsets.only(left: 12.0),
-                                hintText: 'Enter your email here',
-                                hintStyle: TextStyle(color: Colors.grey),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                                  borderSide: BorderSide(
-                                    color: Colors.grey,
-                                    width: 1.0
-                                  )
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                                    borderSide: BorderSide(
-                                        color: Colors.blue,
-                                        width: 2.0
-                                    )
-                                ),
+                  children: [
+                    TextField(
+                        controller: _email,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.only(left: 12.0),
+                          hintText: 'Enter your email here',
+                          hintStyle: TextStyle(color: Colors.grey),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(8.0)),
+                              borderSide: BorderSide(
+                                  color: Colors.grey,
+                                  width: 1.0
                               )
-                            ),
-                            const SizedBox(height: 10),
-                            TextField(
-                                controller: _password,
-                                enableSuggestions:false ,
-                                autocorrect: false,
-                                obscureText: true,
-                                decoration: const InputDecoration(
-                                  contentPadding: EdgeInsets.only(left: 12.0),
-                                  hintText: 'Enter your password here',
-                                  hintStyle: TextStyle(color: Colors.grey),
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                                      borderSide: BorderSide(
-                                          color: Colors.grey,
-                                          width: 1.0
-                                      )
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                                      borderSide: BorderSide(
-                                          color: Colors.blue,
-                                          width: 2.0
-                                      )
-                                  ),
-                                )
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      TextButton(
-                        onPressed: () async {
-                          try{
-                            final email=_email.text;
-                            final password=_password.text;
-                            final userCredential=await FirebaseAuth.instance.signInWithEmailAndPassword(
-                              email: email,
-                              password: password,
-
-                            );
-                          //  print(userCredential);
-                          } on FirebaseAuthException catch (e){
-                            if(e.code=='invalid-credential'){
-                              print('invalid credential, please try again');
-                              
-                            }
-                          }on Exception catch (e){
-                            print(e.toString());
-                          }
-
-
-                        },
-
-                        style:TextButton.styleFrom(
-                          backgroundColor: Colors.blueAccent,
-                          padding: const EdgeInsets.symmetric(vertical: 12.0,horizontal: 32.0),
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(23.0))
                           ),
-                          foregroundColor: Colors.white,
-                          textStyle: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(8.0)),
+                              borderSide: BorderSide(
+                                  color: Colors.blue,
+                                  width: 2.0
+                              )
                           ),
-                          elevation: 5,
-                          shadowColor: Colors.black.withOpacity(0.45),
-                        ),
-                        child: const Text('Sing up'),
+                        )
+                    ),
+                    const SizedBox(height: 10),
+                    TextField(
+                        controller: _password,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.only(left: 12.0),
+                          hintText: 'Enter your password here',
+                          hintStyle: TextStyle(color: Colors.grey),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(8.0)),
+                              borderSide: BorderSide(
+                                  color: Colors.grey,
+                                  width: 1.0
+                              )
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(8.0)),
+                              borderSide: BorderSide(
+                                  color: Colors.blue,
+                                  width: 2.0
+                              )
+                          ),
+                        )
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: () async {
+                  try {
+                    final email = _email.text;
+                    final password = _password.text;
+                    final userCredential = await FirebaseAuth.instance
+                        .signInWithEmailAndPassword(
+                      email: email,
+                      password: password,
 
-                      )
-                    ],
+                    );
+                    print(userCredential);
 
-                  )
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'invalid-credential') {
+                      print('invalid credential, please try again');
+                    }
+                  } on Exception catch (e) {
+                    print(e);
+                  }
+
+                },
+
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 12.0, horizontal: 32.0),
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(23.0))
+                  ),
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18
+                  ),
+                  elevation: 5,
+                  shadowColor: Colors.black.withOpacity(0.45),
+                ),
+                child: const Text('Sing up'),
+
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamedAndRemoveUntil('/register', (route) => false,);
 
 
-                );
+                  },
+                  child: const Text('Not registered yet? Register here!',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    decorationColor: Colors.blue,
+                    decorationThickness: 2,
+                  ),
+                  ),
 
-              default:
-                return const Center(child:  Text('Loading...'));
+              )
+            ],
 
-            }
+          )
 
-
-          },
 
       ),
-
-
     );
-
   }
-
-
 }
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -31,145 +31,140 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title:const Text(
-        'Register',
-        style: TextStyle(
-          color: Colors.white,
-        ),
-
-      ),backgroundColor: Colors.blue,
-        elevation: 5,
-        shadowColor: Colors.black.withOpacity(0.80),
-      ),
-      body:FutureBuilder(
-        future:  Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch(snapshot.connectionState) {
-            case ConnectionState.done:
-              return Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Text(
-                      'Sign Up for Your Accountt',
-
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 22,
-
-                      ),
-                    ),
-                    const SizedBox(height: 30),
-                    Container(
-                      width: 300,
-                      child: Column(
-                        children: [
-                          TextField(
-                            controller: _email,
-                            enableSuggestions: false,
-                            autocorrect: false,
-                            keyboardType: TextInputType.emailAddress,
-                            decoration: const InputDecoration(
-                              contentPadding:EdgeInsets.only(left: 12.0),
-                              hintText: 'Enter your email here',
-                              hintStyle: TextStyle(color: Colors.grey),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                                borderSide: const BorderSide(
-                                  color: Colors.grey,
-                                  width: 1.0,
-                                ),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                                borderSide: const BorderSide(
-                                  color: Colors.blue,
-                                  width: 2.0,
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          TextField(
-                            controller: _password,
-                            enableSuggestions: false,
-                            autocorrect: false,
-                            obscureText: true,
-                            decoration: const InputDecoration(
-                                contentPadding:EdgeInsets.only(left: 12.0),
-                                hintText: 'Enter your password here',
-                                hintStyle: TextStyle(color: Colors.grey),
-                                border:OutlineInputBorder(
-                                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                                    borderSide: BorderSide(
-                                      color: Colors.grey,
-                                      width: 1.0,
-                                    )
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                                    borderSide: BorderSide(
-                                      color: Colors.blue,
-                                      width: 2.0,
-                                    )
-                                )
-
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    TextButton(
-                      onPressed: () async {
-                        try {
-                          final email = _email.text;
-                          final password = _password.text;
-
-                          final userCredential = await FirebaseAuth.instance
-                              .createUserWithEmailAndPassword(
-                              email: email,
-                              password: password
-                          );
-                        } on FirebaseAuthException catch (e){
-                          if(e.code=='channel-error'){
-                            print('Fill in the fields before proceeding');
-                          }else if(e.code=='invalid-email'){
-                            print('Invalid Email');
-                          }else if(e.code=='weak-password'){
-                            print('Invalid password, enter at least 6 characters');
-                          }else if(e.code=='email-already-in-use'){
-                            print('This email is already in use');
-                          }
-                        }
-                      },
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        padding: const EdgeInsets.symmetric(vertical: 12.0,horizontal: 32.0),
-                        shape:const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(23.0)),
+    return Scaffold(
+      appBar: AppBar(title:const Text('Register',style: TextStyle(color: Colors.white),),backgroundColor: Colors.blueAccent,),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'Sign Up for Your Accountt',
+      
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+      
+              ),
+            ),
+            const SizedBox(height: 30),
+            Container(
+              width: 300,
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _email,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 12.0),
+                      hintText: 'Enter your email here',
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        borderSide: const BorderSide(
+                          color: Colors.grey,
+                          width: 1.0,
                         ),
-                        foregroundColor: Colors.white,
-                        textStyle: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18
-                        ),
-                        elevation: 5,
-                        shadowColor: Colors.black.withOpacity(0.45),
                       ),
-
-                      child: const Text('Register'),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        borderSide: const BorderSide(
+                          color: Colors.blue,
+                          width: 2.0,
+                        ),
+                      ),
                     ),
-                  ],
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: _password,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        contentPadding: EdgeInsets.only(left: 12.0),
+                        hintText: 'Enter your password here',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            )
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                            borderSide: BorderSide(
+                              color: Colors.blue,
+                              width: 2.0,
+                            )
+                        )
+      
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: () async {
+                try {
+                  final email = _email.text;
+                  final password = _password.text;
+      
+                  final userCredential = await FirebaseAuth.instance
+                      .createUserWithEmailAndPassword(
+                      email: email,
+                      password: password
+                  );
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'channel-error') {
+                    print('Fill in the fields before proceeding');
+                  } else if (e.code == 'invalid-email') {
+                    print('Invalid Email');
+                  } else if (e.code == 'weak-password') {
+                    print('Invalid password, enter at least 6 characters');
+                  } else if (e.code == 'email-already-in-use') {
+                    print('This email is already in use');
+                  }
+                }
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: const EdgeInsets.symmetric(
+                    vertical: 12.0, horizontal: 32.0),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(23.0)),
                 ),
-              );
-            default:
-              return const Center(child:  Text('Loading...'));
-          }
-        },
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18
+                ),
+                elevation: 5,
+                shadowColor: Colors.black.withOpacity(0.45),
+              ),
+      
+              child: const Text('Register'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamedAndRemoveUntil('/login', (route) => false,);
+
+
+              },
+              child: const Text('Are you registered? Log in to your account!',
+                style: TextStyle(
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.blue,
+                  decorationThickness: 2,
+                ),
+              ),
+
+            )
+          ],
+        ),
       ),
     );
   }
