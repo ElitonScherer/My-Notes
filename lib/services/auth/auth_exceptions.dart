8df309d82448597eb932e5c99e284d41c8434fcc
ElitonScherer <elitonscherer@gmail.com>
2024-12-12 15:59:21 -0400

    step-8

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,10 @@
+class ChannelErrorAuthException implements Exception{}
+class InvalidCredentialAuthException implements Exception{}
+class InvalidEmailAuthException implements Exception{}
+
+class WeakPasswordAuthException implements Exception{}
+class EmailAlreadyInUseAuthException implements Exception{}
+
+
+class GenericAuthException implements Exception{}
+class UserNotLoggedInAuthException implements Exception{}
